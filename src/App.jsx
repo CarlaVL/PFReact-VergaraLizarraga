a4665b2956059b23adfe67de2245fe927cd2c0f5
Carla Vergara <vergaracarlar@gmail.com>
2024-01-16 23:49:37 -0300

    agregue itemcount

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import NavBAr from './components/NavBar/NavBar'
 import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner'
+import ItemCount from './components/ItemCount/ItemCount'
 import CartWidget from './components/CartWidget/CartWidget'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <NavBAr />
       <ItemListConteiner greeting= {'Bienvenidos!'}/>
-    </>
+      <ItemCount initial= {0} stock={10} onAdd={(quantity) => console.log ('Cantidad agregada ',quantity)}/>
+    </div>
   )
 }
 
